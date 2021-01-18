a2f39424c87afe6a7c54df576a3a8518b957e199
Anastasia D'Ausilio <adausilio@usf.edu>
2021-01-18 15:29:33 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pause to display Hello World;
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
