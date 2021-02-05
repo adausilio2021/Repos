a5c057681ce94a3dac557dc80d2032a8ff927804
adausilio2021 <77417101+adausilio2021@users.noreply.github.com>
2021-02-04 22:13:16 -0500

    Add files via upload
    Arrays

--- Program.cs
+++ Program.cs
@@ -1,48 +1,47 @@
-﻿//Anastasia D'Ausilio
-// Due 1/29/2021
-// ISM4300
-// Deliverable 2:Iterative Statements
+﻿//Anastasia D'Ausilio  
+//Due 2/5/2021
+//ISM 4300
+//Deliverable 3 Arrays
+
+
 using System;
 
-namespace Iteration_Statement
+namespace Arrays
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Ask for user input
-            Console.WriteLine("Enter an integer between 1 and 100");
+            int[] numbersArray;
+            numbersArray = new int[25];
+            Console.WriteLine(
+            numbersArray[0] = 1;
+            numbersArray[1] = 2;
+            numbersArray[2] = 3;
+            numbersArray[3] = 4;
+            numbersArray[4] = 5;
+            numbersArray[5] = 6;
+            numbersArray[6] = 7;
+            numbersArray[7] = 8;
+            numbersArray[8] = 9;
+            numbersArray[9] = 10;
+            numbersArray[10] = 11;
+            numbersArray[11] = 12;
+            numbersArray[12] = 13;
+            numbersArray[13] = 14;
+            numbersArray[14] = 15;
+            numbersArray[15] = 16;
+            numbersArray[16] = 17;
+            numbersArray[17] = 18;
+            numbersArray[18] = 19;
+            numbersArray[19] = 20;
+            numbersArray[20] = 21;
+            numbersArray[21] = 22;
+            numbersArray[22] = 23;
+            numbersArray[23] = 24;
+            numbersArray[24] = 25;
+            );
 
-            //use try catch to validate input
-            //if user provides bad input, the catch block will handel
-            try
-            {
-                string input = Console.ReadLine();
-
-                //value_of_input is the variable
-                int value_of_input = int.Parse(input);
-                //allow integer value of 1-100
-                if ((value_of_input >= 1) && (value_of_input <= 100))
-                {
-                    Console.WriteLine("You have entered " + value_of_input.ToString());
-                    //for loop
-                    for (int i = 0; i < value_of_input; i++)
-                    {
-                        Console.WriteLine
-                            ("This is the current integer value in the loop: " +
-                          i.ToString());
-                    }
-                    Console.WriteLine("Press any key to exit the program");
-                    Console.ReadKey(true);
-                }
-            }
-            catch
-            {
-                Console.WriteLine("Please enter integer value and try running the program again");
-                Console.WriteLine("Press any key to exit the program");
-                Console.ReadKey(true);
-            }
-            }
-            }
+        }
     }
-
+}
